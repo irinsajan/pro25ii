21842de3f65d5170953eb1f8758aca22930a5f8e
irinsajan <69659076+irinsajan@users.noreply.github.com>
2020-12-12 08:52:47 +0530

    Update sketch.js
    size changes

--- sketch.js
+++ sketch.js
@@ -20,9 +20,9 @@ function setup() {
 	//Create the Bodies Here.
 	ball = new Ball(200,200,40);
 	ground = new Ground(750,height,1500,20);
-	box1 = new Dustbin(1415,690,70,20);
-	box2 = new Dustbin(1450,650,20,70);
-	box3 = new Dustbin(1390,650,20,50);
+	box1 = new Dustbin(1410,690,120,20);
+	box2 = new Dustbin(1470,650,20,70);
+	box3 = new Dustbin(1350,650,20,70);
 
 	//keyPressed ();
 
@@ -39,13 +39,13 @@ box2.display();
 box3.display();
 ground.display();
 ball.display();
-image(dustbinImage,1420,650,100,100);
+image(dustbinImage,1410,630,160,120);
 //background(1);
  
 }
 function keyPressed (){
 	if (keyCode === UP_ARROW){
 		//Matter.Body.applyForce(paperObject.Body,paperObject.Body.position,{x:85,y:-85});
-		Matter.Body.applyForce(ball.body,ball.body.position,{x:400,y:-400});
+		Matter.Body.applyForce(ball.body,ball.body.position,{x:97,y:-97});
 	}
 }
